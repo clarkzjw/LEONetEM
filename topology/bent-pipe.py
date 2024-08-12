d2ebf17cadf2f474e0376182b8c8cc63247fa73c
clarkzjw <i@jinwei.me>
2024-08-12 14:31:42 -0700

    simple bent-pipe

--- topology/bent-pipe.py
+++ topology/bent-pipe.py
@@ -23,34 +23,6 @@ line_lock = Lock()
 update_event = threading.Event()
 file_counter = Value('i', 0)
 
-def auto_test():
-    for i in range(50):
-        h1.sendCmd('xterm -title "node: h1 server" -hold -e "./picoquicdemo -M 1 -p 4434 -G bbr1 -q ./server_qlog -w ./sample/" &')
-        h2.sendCmd('xterm -title "node: h2 client" -hold -e "./picoquicdemo -n test -M 1 -A 10.0.5.3/3 -G bbr1 -q ./client_qlog -o /usr 10.0.1.2 4434 /testfile_1" &')
-        time.sleep(145)
-        h1.sendInt()
-        h2.sendInt()
-        h1.waitOutput()
-        h2.waitOutput()
-        time.sleep(5)
-        if i == 49:
-            print(f"line_number_5g.value: {line_number_5g.value}")
-            print(f"line_number_starlink.value: {line_number_starlink.value}")
-
-class NetworkConfigThread(threading.Thread):
-    def __init__(self, net, host_name, dev, column, barrier, line_number, line_lock, update_event):
-        super().__init__()
-        self.net = net
-        self.host_name = host_name
-        self.column = column
-        self.dev = dev
-        self.barrier = barrier
-        self.line_number = line_number
-        self.line_lock = line_lock
-        self.update_event = update_event
-
-    def run(self):
-        configureNetworkConditions(self.net, self.host_name, self.dev, self.column, self.barrier, self.line_number, self.line_lock, self.update_event)
 
 class NetworkConfigThread_Starlink(threading.Thread):
     def __init__(self, net, host_name, dev, column, barrier, line_number, line_lock, update_event):
@@ -87,7 +59,7 @@ def configureNetworkConditions_starlink(net, host_name, dev, column, barrier, li
 
         if not init_flags[dev]:
             init_flags[dev] = True
-            check_and_start_test()
+            # check_and_start_test()
 
     barrier.wait()
 
@@ -106,51 +78,6 @@ def configureNetworkConditions_starlink(net, host_name, dev, column, barrier, li
             host.cmd(update_cmd)
 
         barrier.wait()
-
-def configureNetworkConditions(net, host_name, dev, column, barrier, line_number, line_lock, update_event):
-    global init_flags
-
-    with open('./5G.csv', 'r') as file:
-        reader = csv.reader(file)
-        latency_lines = list(reader)
-
-    host = net.get(host_name)
-
-    with line_lock:
-        initialBW = float(latency_lines[line_number.value][column - 2])
-        cmd_bw = 'tc qdisc replace dev {} root handle 1: tbf rate {}mbit burst 15k latency 50ms'.format(dev, initialBW)
-        host.cmd(cmd_bw)
-
-        initialDelay = float(latency_lines[line_number.value][column])
-        cmd_jitter = 'tc qdisc add dev {} parent 1:1 handle 10: netem delay {}ms loss 1%'.format(dev, initialDelay)
-        host.cmd(cmd_jitter)
-
-        if not init_flags[dev]:
-            init_flags[dev] = True
-            check_and_start_test()
-
-    barrier.wait()
-
-    while True:
-        update_event.wait()
-        update_event.clear()
-
-        with line_lock:
-            current_line_number = line_number.value
-            currentBW = float(latency_lines[current_line_number][column - 2])
-            update_cmd_bw = 'tc qdisc change dev {} root handle 1: tbf rate {}mbit burst 15k latency 50ms'.format(dev, currentBW)
-            host.cmd(update_cmd_bw)
-
-            currentDelay = float(latency_lines[current_line_number][column])
-            update_cmd = 'tc qdisc change dev {} parent 1:1 handle 10: netem delay {}ms loss 1%'.format(dev, currentDelay)
-            host.cmd(update_cmd)
-
-        barrier.wait()
-
-def check_and_start_test():
-    if all(init_flags.values()):
-        test_process = Process(target=auto_test)
-        test_process.start()
 
 def update_lines_periodically(scheduler, step, start_time):
     global line_number_5g, line_number_starlink
@@ -275,16 +202,9 @@ if '__main__' == __name__:
 
     h2.cmd("ip route add default scope global nexthop via 10.0.4.2 dev h2-eth0")
 
-    h2.cmd('xterm -title "node: h2 monitoring" -hold -e "sudo bwm-ng" &')
-
-    network_thread1 = NetworkConfigThread(net, 'r3', 'r3-eth1', 2, barrier, line_number_5g, line_lock, update_event)
-    network_thread3 = NetworkConfigThread(net, 'r5', 'r5-eth0', 3, barrier, line_number_5g, line_lock, update_event)
-
     network_thread2 = NetworkConfigThread_Starlink(net, 'r2', 'r2-eth1', 3, barrier, line_number_starlink, line_lock, update_event)
     network_thread4 = NetworkConfigThread_Starlink(net, 'r4', 'r4-eth0', 2, barrier, line_number_starlink, line_lock, update_event)
 
-    network_thread1.start()
-    network_thread3.start()
     network_thread2.start()
     network_thread4.start()
 
