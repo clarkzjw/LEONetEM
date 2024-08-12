29ec83c8a9c22072fc0af6df9f6329f79d7cbe20
clarkzjw <i@jinwei.me>
2024-08-12 14:59:32 -0700

    add simplified topology

--- topology/bent-pipe.py
+++ topology/bent-pipe.py
@@ -1,3 +1,8 @@
+"""
+h1 - r - PoP - h2
+client - router - PoP - server
+"""
+
 import time
 import csv
 import threading
@@ -100,119 +105,56 @@ if '__main__' == __name__:
     setLogLevel('info')
     net = Mininet(link=TCLink)
 
-    h1 = net.addHost('h1')
-    h2 = net.addHost('h2')
-    r1 = net.addHost('r1')
-    r2 = net.addHost('r2')
-    r3 = net.addHost('r3')
-    r4 = net.addHost('r4')
-    r5 = net.addHost('r5')
+    client = net.addHost('client')
+    server = net.addHost('server')
+    router = net.addHost('router')
+    pop = net.addHost('pop')
 
-    net.addLink(r1, h1, cls=TCLink)
-    net.addLink(r1, r4, cls=TCLink)
-    net.addLink(r1, r5, cls=TCLink)
-    net.addLink(r4, r2, cls=TCLink)
-    net.addLink(r5, r3, cls=TCLink)
-    net.addLink(r2, h2, cls=TCLink)
-    net.addLink(r3, h2, cls=TCLink)
+    net.addLink(client, router, cls=TCLink)
+    net.addLink(router, pop, cls=TCLink)
+    net.addLink(pop, server, cls=TCLink)
     net.build()
 
-    r1.cmd("ifconfig r1-eth0 0")
-    r1.cmd("ifconfig r1-eth1 0")
-    r1.cmd("ifconfig r1-eth2 0")
+    client.cmd("ifconfig client-eth0 0")
+    client.cmd("ifconfig client-eth0 192.168.1.101 netmask 255.255.255.0")
+    client.cmd("ip route add default scope global nexthop via 192.168.1.1 dev client-eth0")
+    client.cmd("ip route add 10.10.10.0/24 via 192.168.1.1")
 
-    r1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r1.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
+    router.cmd("ifconfig router-eth0 0")
+    router.cmd("ifconfig router-eth1 0")
+    router.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    router.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
+    router.cmd("ifconfig router-eth0 192.168.1.1 netmask 255.255.255.0")
+    # 100.64.0.0/10
+    # assume the WAN side IP of the router is 100.76.100.1
+    router.cmd("ifconfig router-eth1 100.76.100.1 netmask 255.192.0.0")
+    router.cmd("ip route add default scope global nexthop via 100.64.0.1 dev router-eth0")
+    router.cmd("ip route add 10.10.10.0/24 via 100.64.0.1")
 
-    r1.cmd("ifconfig r1-eth0 10.0.1.1 netmask 255.255.255.0")
-    r1.cmd("ifconfig r1-eth1 10.0.2.1 netmask 255.255.255.0")
-    r1.cmd("ifconfig r1-eth2 10.0.3.1 netmask 255.255.255.0")
+    pop.cmd("ifconfig pop-eth0 0")
+    pop.cmd("ifconfig pop-eth1 0")
+    pop.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    pop.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
+    pop.cmd("ifconfig pop-eth0 100.64.0.1 netmask 255.192.0.0")
+    # the link between PoP and server is simplified
+    pop.cmd("ifconfig pop-eth1 10.10.10.1 netmask 255.255.255.0")
+    pop.cmd("ip route add 192.168.1.0/24 via 100.76.100.1")
 
-    r4.cmd("ifconfig r4-eth0 0")
-    r4.cmd("ifconfig r4-eth1 0")
+    server.cmd("ifconfig server-eth0 0")
+    server.cmd("ifconfig server-eth0 10.10.10.101 netmask 255.255.255.0")
+    server.cmd("ip route add default scope global nexthop via 10.10.10.101 dev server-eth0")
 
-    r4.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r4.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
+    # network_thread2 = NetworkConfigThread_Starlink(net, 'r2', 'r2-eth1', 3, barrier, line_number_starlink, line_lock, update_event)
+    # network_thread4 = NetworkConfigThread_Starlink(net, 'r4', 'r4-eth0', 2, barrier, line_number_starlink, line_lock, update_event)
 
-    r4.cmd("ifconfig r4-eth0 10.0.2.4 netmask 255.255.255.0")
-    r4.cmd("ifconfig r4-eth1 10.0.6.4 netmask 255.255.255.0")
+    # network_thread2.start()
+    # network_thread4.start()
 
-    r5.cmd("ifconfig r5-eth0 0")
-    r5.cmd("ifconfig r5-eth1 0")
-
-    r5.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r5.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
-
-    r5.cmd("ifconfig r5-eth0 10.0.3.4 netmask 255.255.255.0")
-    r5.cmd("ifconfig r5-eth1 10.0.7.4 netmask 255.255.255.0")
-
-    r2.cmd("ifconfig r2-eth0 0")
-    r2.cmd("ifconfig r2-eth1 0")
-
-    r2.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r2.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
-
-    r2.cmd("ifconfig r2-eth0 10.0.6.2 netmask 255.255.255.0")
-    r2.cmd("ifconfig r2-eth1 10.0.4.2 netmask 255.255.255.0")
-
-    r3.cmd("ifconfig r3-eth0 0")
-    r3.cmd("ifconfig r3-eth1 0")
-
-    r3.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    r3.cmd("echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp")
-
-    r3.cmd("ifconfig r3-eth0 10.0.7.2 netmask 255.255.255.0")
-    r3.cmd("ifconfig r3-eth1 10.0.5.2 netmask 255.255.255.0")
-
-    r2.cmd("ip route add 10.0.1.0/24 via 10.0.6.4")
-    r3.cmd("ip route add 10.0.1.0/24 via 10.0.7.4")
-
-    r1.cmd("ip route add 10.0.4.0/24 via 10.0.2.4")
-    r1.cmd("ip route add 10.0.5.0/24 via 10.0.3.4")
-
-    r4.cmd("ip route add 10.0.1.0/24 via 10.0.2.1")
-    r5.cmd("ip route add 10.0.1.0/24 via 10.0.3.1")
-    r4.cmd("ip route add 10.0.4.0/24 via 10.0.6.2")
-    r5.cmd("ip route add 10.0.5.0/24 via 10.0.7.2")
-
-    h1.cmd("ifconfig h1-eth0 0")
-
-    h2.cmd("ifconfig h2-eth0 0")
-    h2.cmd("ifconfig h2-eth1 0")
-
-    h1.cmd("ifconfig h1-eth0 10.0.1.2 netmask 255.255.255.0")
-
-    h2.cmd("ifconfig h2-eth0 10.0.4.3 netmask 255.255.255.0")
-    h2.cmd("ifconfig h2-eth1 10.0.5.3 netmask 255.255.255.0")
-
-    h1.cmd("ip route add default scope global nexthop via 10.0.1.1 dev h1-eth0")
-
-    h2.cmd("ip rule add from 10.0.4.3 table 1")
-    h2.cmd("ip rule add from 10.0.5.3 table 2")
-
-    h2.cmd("ip route add 10.0.6.0/24 dev h2-eth0 table 1")
-    h2.cmd("ip route add 10.0.4.0/24 dev h2-eth0 table 1")
-    h2.cmd("ip route add 10.0.2.0/24 dev h2-eth0 table 1")
-    h2.cmd("ip route add 10.0.1.0/24 dev h2-eth0 table 1")
-
-    h2.cmd("ip route add 10.0.7.0/24 dev h2-eth1 table 2")
-    h2.cmd("ip route add 10.0.5.0/24 dev h2-eth1 table 2")
-    h2.cmd("ip route add 10.0.3.0/24 dev h2-eth1 table 2")
-    h2.cmd("ip route add 10.0.1.0/24 dev h2-eth1 table 2")
-
-    h2.cmd("ip route add default scope global nexthop via 10.0.4.2 dev h2-eth0")
-
-    network_thread2 = NetworkConfigThread_Starlink(net, 'r2', 'r2-eth1', 3, barrier, line_number_starlink, line_lock, update_event)
-    network_thread4 = NetworkConfigThread_Starlink(net, 'r4', 'r4-eth0', 2, barrier, line_number_starlink, line_lock, update_event)
-
-    network_thread2.start()
-    network_thread4.start()
-
-    scheduler = sched.scheduler(time.time, time.sleep)
-    start_time = time.perf_counter()
-    scheduler.enter(0.1, 1, update_lines_periodically, (scheduler, 0.1, start_time))
-    update_thread = threading.Thread(target=scheduler.run)
-    update_thread.start()
+    # scheduler = sched.scheduler(time.time, time.sleep)
+    # start_time = time.perf_counter()
+    # scheduler.enter(0.1, 1, update_lines_periodically, (scheduler, 0.1, start_time))
+    # update_thread = threading.Thread(target=scheduler.run)
+    # update_thread.start()
 
     CLI(net)
     net.stop()
